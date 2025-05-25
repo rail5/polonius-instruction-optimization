ef69ff79b120efee9bc69462c09598ec1362f1fc
rail5 <andrew@rail5.org>
2025-05-25 15:44:38 +0800

    With theorem #1 applied, -O1 optimizations are complete
    More testing still needs to be done to verify that this is implemented correctly

--- expression.cpp
+++ expression.cpp
@@ -44,9 +44,9 @@ Expression Expression::operator+(Block block) {
 				}
 			}
 			for (const auto& b : inserts) {
-					blocks.push_back(b);
-					operators.push_back(ADD);
-				}
+				blocks.push_back(b);
+				operators.push_back(ADD);
+			}
 		case 0:
 			blocks.push_back(block);
 			operators.push_back(ADD);
@@ -65,15 +65,57 @@ Expression Expression::operator+(Block block) {
 }
 
 Expression Expression::operator-(Block block) {
+	std::deque<Block> inserts;
+	std::deque<Block> removes;
+	std::deque<Block> replaces;
 	switch (optimization_level) {
 		default:
 		case 3:
 		case 2:
 		case 1:
+			// Level 1 optimizations:
+			// Apply theorem #1 (combining remove instructions)
+			// Most of the leg-work is already handled by INSERTs (above)
+			// Here, we only have to make sure we separate REMOVEs from REPLACEs
+			while (blocks.size() > 0) {
+				switch (operators[0]) {
+					case ADD:
+						inserts.push_back(blocks[0]);
+						blocks.pop_front();
+						operators.pop_front();
+						break;
+					case SUBTRACT:
+						removes.push_back(blocks[0]);
+						blocks.pop_front();
+						operators.pop_front();
+						break;
+					case MULTIPLY:
+						replaces.push_back(blocks[0]);
+						blocks.pop_front();
+						operators.pop_front();
+						if (replaces.back().start() >= block.start()) {
+							replaces.back().shift_left(block.size());
+						}
+						break;
+				}
+			}
+			for (const auto& b : inserts) {
+				blocks.push_back(b);
+				operators.push_back(ADD);
+			}
+			for (const auto& b : removes) {
+				blocks.push_back(b);
+				operators.push_back(SUBTRACT);
+			}
 		case 0:
 			blocks.push_back(block);
 			operators.push_back(SUBTRACT);
 			break;
+	}
+	// Copy 'replaces' to the main blocks vector
+	for (const auto& b : replaces) {
+		blocks.push_back(b);
+		operators.push_back(MULTIPLY);
 	}
 	return *this;
 }
