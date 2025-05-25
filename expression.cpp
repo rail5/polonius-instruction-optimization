7ba78a3130941c7dcf63d4ae2b324bb574c9f027
rail5 <andrew@rail5.org>
2025-05-25 18:30:35 +0800

    Properly handle overlaps between replace/remove instructions
    Since we're organizing instructions into sequences of (all inserts) -> (all removes) -> (all replaces), it's necessary for us to check whether the original (given) instruction sequence has a replace instruction which replaces data at a position which is later removed.
    
    If so, we eliminate the redundant portion of the replace instruction and shift its position to compensate for the newly-preceding remove

--- expression.cpp
+++ expression.cpp
@@ -10,6 +10,8 @@ Expression Expression::operator+(Block block) {
 		default:
 		case 3:
 		case 2:
+			// Level 2 optimizations:
+			// Apply theorem #3 (eliminating redundant insert/remove pairs)
 		case 1:
 			// Level 1 optimizations:
 			// Apply theorem #0 (combining insert instructions)
@@ -72,12 +74,15 @@ Expression Expression::operator-(Block block) {
 		default:
 		case 3:
 		case 2:
+			// Level 2 optimizations:
+			// Apply theorem #4 (eliminating redundant insert/remove pairs)
 		case 1:
 			// Level 1 optimizations:
 			// Apply theorem #1 (combining remove instructions)
 			// Most of the leg-work is already handled by INSERTs (above)
 			// Here, we only have to make sure we separate REMOVEs from REPLACEs
 			while (blocks.size() > 0) {
+				std::pair<uint64_t, uint64_t> overlap;
 				switch (operators[0]) {
 					case ADD:
 						inserts.push_back(blocks[0]);
@@ -93,8 +98,20 @@ Expression Expression::operator-(Block block) {
 						replaces.push_back(blocks[0]);
 						blocks.pop_front();
 						operators.pop_front();
-						if (replaces.back().start() >= block.start()) {
+						overlap = replaces.back().overlap(block);
+						if (replaces.back().start() > block.end()) {
 							replaces.back().shift_left(block.size());
+						} else if (overlap != std::pair<uint64_t, uint64_t>(0, 0)) {
+							// Remove the overlapping portion of the REPLACE instruction
+							replaces.back().remove(overlap.first, overlap.second);
+							if (replaces.back().empty()) {
+								// If the REPLACE instruction is now empty, remove it
+								replaces.pop_back();
+							} else {
+								// If it's not empty, shift it left to compensate for the removed portion
+								// (Since this replace will be put *after* the REMOVE instruction)
+								replaces.back().shift_left(overlap.second - overlap.first + 1);
+							}
 						}
 						break;
 				}
